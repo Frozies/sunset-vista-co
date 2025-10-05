526b3c2316219478b270593c65309af5331c9d9a
Davin <52686922+Frozies@users.noreply.github.com>
2025-10-04 23:18:50 -0400

    move analytics and speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,48 +1,47 @@
-import { Inter } from "next/font/google";
+import {Inter, Rubik, Taviraj} from "next/font/google";
 import "./globals.css";
-import { Taviraj } from 'next/font/google'
-import { Rubik } from 'next/font/google'
-import Head from "next/head";
 import Script from "next/script";
-import { SpeedInsights } from "@vercel/speed-insights/next"
-const inter = Inter({ subsets: ["latin"] });
-import { Analytics } from "@vercel/analytics/next"
+import {SpeedInsights} from "@vercel/speed-insights/next"
+import {Analytics} from "@vercel/analytics/next"
+
+const inter = Inter({subsets: ["latin"]});
+
 const taviraj = Taviraj({
-  subsets: ['latin'],
-  display: 'swap',
-  weight: "400",
-  variable: '--font-taviraj'
+    subsets: ['latin'],
+    display: 'swap',
+    weight: "400",
+    variable: '--font-taviraj'
 })
 
 const rubik = Rubik({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-rubik',
+    subsets: ['latin'],
+    display: 'swap',
+    variable: '--font-rubik',
 })
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <head>
-          <Analytics/>
-          <SpeedInsights/>
-        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-SKLCFXSXKY" strategy="afterInteractive" />
-        <Script id="gtag-init" strategy="afterInteractive">
-          {`
+    return (
+        <html lang="en">
+        <Analytics/>
+        <SpeedInsights/>
+        <head>
+            <Script async src="https://www.googletagmanager.com/gtag/js?id=G-SKLCFXSXKY" strategy="afterInteractive"/>
+            <Script id="gtag-init" strategy="afterInteractive">
+                {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
             gtag('config', 'G-SKLCFXSXKY');
           `}
-        </Script>
-      </head>
-      <body className={inter.className + ' ' + taviraj.variable + ' ' + rubik.variable}>{children}</body>
-    </html>
-  );
+            </Script>
+        </head>
+        <body className={inter.className + ' ' + taviraj.variable + ' ' + rubik.variable}>{children}</body>
+        </html>
+    );
 }
 
 
